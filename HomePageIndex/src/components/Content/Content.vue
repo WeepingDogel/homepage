cedee379f65394d72d09dfa6ea1cddf62f8aff5f
WeepingDogel <weepingdogel@gmail.com>
2023-01-31 00:36:10 +0800

    Vue Components.

---
+++ HomePageIndex/src/components/Content/Content.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ContentBG">
+    </div>
+</template>
+
+<style>
+.ContentBG{
+    width: 100%;
+    height: auto;
+    min-height: 800px;
+    background-color: var(--nord4);
+}
+</style>
+
+<script>
+
+</script>
---
+++ HomePageIndex/src/components/TopBar/TopBar.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="TopBar">
+    <NavBar />
+</div>
+</template>
+
+<style>
+.TopBar{
+    width: 100%;
+    height: 100px;
+    background-color: var(--nord1);
+    margin-bottom: 10px;
+}
+</style>
+
+<script>
+import NavBar from './Navbar.vue'
+export default{
+    components:{
+        NavBar
+    }
+}
+</script>
