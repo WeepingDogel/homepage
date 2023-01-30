8e625e7784172b0b8931d6f7fb35318d3524fe32
WeepingDogel <weepingdogel@gmail.com>
2023-01-31 00:35:17 +0800

    Vue ready.

---
+++ HomePageIndex/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <TopBar />
+      <Content />
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import TopBar from './components/TopBar/TopBar.vue'
+import Content from './components/Content/Content.vue'
+export default{
+  components:{
+    TopBar,
+    Content
+  }
+}
+</script>
